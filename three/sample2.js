c8a8a5c3e50b4c8cb957b6720533458093b87e81
kojiyamauchi <all.the.small.things@me.com>
2017-04-08 16:06:30 +0900

    upDate three/sample2.js #2

--- three/sample2.js
+++ three/sample2.js
@@ -5,12 +5,14 @@ if(document.getElementById('sample2')) {
 
     var draw = function () {
 
-        var scale = chroma.scale(['#ffffff', '#7037e8', '#e8574d', '#459a16']);
         // WebGL - three.js Code.
         // THREE.Scene(); => putiing objects.
         // THREE.PerspectiveCamera(); => photograph the object.
         // THREE.WebGLRenderer(); => render the camera.
         // View => draw to the canvas.
+
+        // Using Chroma.js
+        var scale = chroma.scale(['#ffffff', '#7037e8', '#e8574d', '#459a16']);
 
         // Variable for Creating Object & Light Source.
         var scene = new THREE.Scene();
