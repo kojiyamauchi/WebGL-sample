ae95591322e5b8e768c4bca35e5b9bb5cee01e63
kojiyamauchi <all.the.small.things@me.com>
2017-04-30 21:54:04 +0900

    up date gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-var gulp = require("gulp"), // call gulp.
+var gulp = require('gulp'), // call gulp.
     browserify = require('browserify'), // Require Browserify.
     riotify = require('riotify'), // Riot.js Compile.
     source = require('vinyl-source-stream'), // Need To Use Browserify.
-    plumber = require("gulp-plumber"), // case, error task. don't stop watch plugin.
-    sass = require("gulp-sass"), // sass file compile plugin.
-    sourcemaps = require("gulp-sourcemaps"), // write sourcemaps.
-    gutil = require("gulp-util"), // gulp-util plugin.
-    imageMin = require("gulp-imagemin"), // images compression plugin.
-    pngImageMin = require("imagemin-pngquant"), // png images compression plugin.
-    changed = require("gulp-changed"), // only change file watch plugin.
-    noCompressionImagesFold = (["noCompressionImages/*.jpg", "noCompressionImages/*.jpeg", "noCompressionImages/*.png", "noCompressionImages/*.gif", "noCompressionImages/*.svg"]), // no compression images fold.
-    compressionImageFold = "images/", // finish compression images fold.
-    autoprefixer = require("gulp-autoprefixer"), // add vendor prefix in CSS automatically.
+    plumber = require('gulp-plumber'), // case, error task. don't stop watch plugin.
+    sass = require('gulp-sass'), // sass file compile plugin.
+    sourcemaps = require('gulp-sourcemaps'), // write sourcemaps.
+    gutil = require('gulp-util'), // gulp-util plugin.
+    imageMin = require('gulp-imagemin'), // images compression plugin.
+    pngImageMin = require('imagemin-pngquant'), // png images compression plugin.
+    changed = require('gulp-changed'), // only change file watch plugin.
+    noCompressionImagesFold = (['noCompressionImages/*.jpg', 'noCompressionImages/*.jpeg', 'noCompressionImages/*.png', 'noCompressionImages/*.gif', 'noCompressionImages/*.svg']), // no compression images fold.
+    compressionImageFold = 'images/', // finish compression images fold.
+    autoprefixer = require('gulp-autoprefixer'), // add vendor prefix in CSS automatically.
     cssmin = require('gulp-cssmin'), // CSS File Compression.
     jsmin = require('gulp-uglify'), // JS File Compression.
-    rename = require("gulp-rename"), // File Rename PlugIn.
-    del = require("del"), // File Delete, Not Gulp PlugIn.
-    ftp = require("vinyl-ftp"), // ftp plugin.
-    sftp = require("gulp-sftp"), // sftp plugin.
-    using_PHP_LocalServerConnect = require("gulp-connect-php"), // using php local server connect plugin.
-    browserSync = require("browser-sync"), // local browser sync plugin.
-    upLoadFileWrite = (["*.html", "css/default.min.css", "js/core.min.js", "images/*"]), // upload file write.
+    rename = require('gulp-rename'), // File Rename PlugIn.
+    del = require('del'), // File Delete, Not Gulp PlugIn.
+    ftp = require('vinyl-ftp'), // ftp plugin.
+    sftp = require('gulp-sftp'), // sftp plugin.
+    using_PHP_LocalServerConnect = require('gulp-connect-php'), // using php local server connect plugin.
+    browserSync = require('browser-sync'), // local browser sync plugin.
+    upLoadFileWrite = (['*.html', 'css/default.min.css', 'js/core.min.js', 'images/*']), // upload file write.
     notUpLoadFileWrite = ([]), // don't upload file write.
     upLoadFile = upLoadFileWrite.concat(notUpLoadFileWrite); //ftp upload file. variable upLoadFileWrite concatenate variable notUpLoadFileWrite.
 
@@ -32,18 +32,21 @@ gulp.task('browserify', function () {
         })
         .transform([riotify])
         .bundle()
+        .on('error', function (err) {
+            console.log('Error : ' + err.message);
+        })
         .pipe(source('core.js'))
         .pipe(gulp.dest('js/'));
 });
 
 // compression images.
-gulp.task("compressionImages", function () {
+gulp.task('compressionImages', function () {
     gulp.src(noCompressionImagesFold)
         .pipe(plumber())
         .pipe(changed(compressionImageFold))
         .pipe(imageMin({
             use: [pngImageMin({
-                quality: "60-80",
+                quality: '60-80',
                 speed: 4
             })]
         }))
@@ -51,25 +54,25 @@ gulp.task("compressionImages", function () {
 });
 
 // sass compile.
-gulp.task("sass", function () {
-    gulp.src("sass/default.scss")
+gulp.task('sass', function () {
+    gulp.src('sass/default.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
         }))
-        .pipe(sourcemaps.write("../maps"))
-        .pipe(gulp.dest("css/"));
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('css/'));
 });
 
 // add vendor prefix automatically.
-gulp.task("autoprefixer", function () {
-    gulp.src("css/default.css")
+gulp.task('autoprefixer', function () {
+    gulp.src('css/default.css')
         .pipe(autoprefixer({
-            browsers: ["last 2 versions", "ie >= 9", "Android >= 4", "ios_saf >= 8"],
+            browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
             cascade: false
         }))
-        .pipe(gulp.dest("css/"));
+        .pipe(gulp.dest('css/'));
 });
 
 // CSS File Compression.
@@ -95,7 +98,7 @@ gulp.task('jsmin', function () {
 });
 
 // HTML File Rename PHP File. Setting at The Work Start.
-gulp.task("rename", function () {
+gulp.task('rename', function () {
     gulp.src('index.html')
         .pipe(rename({
             extname: '.php'
@@ -104,57 +107,57 @@ gulp.task("rename", function () {
 });
 
 // HTML File & .DS_Store Delete. Setting at The Work Start.
-gulp.task("delete", function (cb) {
-    del(["index.html", "**/.DS_Store"], cb);
+gulp.task('delete', function (cb) {
+    del(['index.html', '**/.DS_Store'], cb);
 });
 
 // local browser connect & sync.
-gulp.task("browserSync", function () {
+gulp.task('browserSync', function () {
     using_PHP_LocalServerConnect.server({
         port: 8080,
-        bin: "/Applications/MAMP/bin/php/php5.6.10/bin/php", // PHP pass.
-        ini: "/Applications/MAMP/bin/php/php5.6.10/conf/php.ini" // PHP.ini pass.
+        bin: '/Applications/MAMP/bin/php/php5.6.10/bin/php', // PHP pass.
+        ini: '/Applications/MAMP/bin/php/php5.6.10/conf/php.ini' // PHP.ini pass.
     }, function () {
         browserSync({
-            proxy: "localhost:8080",
+            proxy: 'localhost:8080',
             notify: false,
-            browser: "google chrome"
+            browser: 'google chrome'
         });
     });
 });
 
 // file save's local browser reload.
-gulp.task("localBrowserReload", function () {
+gulp.task('localBrowserReload', function () {
     browserSync.reload();
 });
 
 // ftp upload.
-gulp.task("ftpUpLoad", function () {
+gulp.task('ftpUpLoad', function () {
     var ftpConnect = ftp.create({
-        host: "***",
-        user: "***",
-        password: "***",
+        host: '***',
+        user: '***',
+        password: '***',
         parallel: 7,
         log: gutil.log
     });
     gulp.src(upLoadFile, {
-            base: ".",
+            base: '.',
             buffer: false
         })
-        .pipe(ftpConnect.newer("/webGL_sample"))
-        .pipe(ftpConnect.dest("/webGL_sample"));
+        .pipe(ftpConnect.newer('/webGL_sample'))
+        .pipe(ftpConnect.dest('/webGL_sample'));
 });
 
-// gulp default task, terminal command "gulp".
-gulp.task("default", ["browserSync"], function () { // first task, local server connect & local browser sync.
-    gulp.watch(["base/*", "tags/*", 'three/*'], ["browserify"]); // JS File Browserify.
-    gulp.watch("js/core.js", ["jsmin"]); // watching change's JS flie, File Compression.
-    gulp.watch("sass/default.scss", ["sass"]); // watching sass file save's auto compile.
-    gulp.watch("css/default.css", ["autoprefixer"]); // watching change's CSS flie. add vendor prefix automatically.
-    gulp.watch("css/default.css", ["cssmin"]); // watching change's CSS flie, File Compression.
-    gulp.watch(noCompressionImagesFold, ["compressionImages"]); // watching noCompressionImages fold changed images, compression images.
-    //gulp.watch("**/*", ["rename"]); // watching change's HTML flie. Rename PHP file.
-    //gulp.watch("**/*", ["delete"]); // watching rename PHP file. delet HTML file.
-    gulp.watch(upLoadFile, ["ftpUpLoad"]); // watching file save's auto ftp upload.
-    gulp.watch(upLoadFile, ["localBrowserReload"]); // watching file save's local browser reload.
+// gulp default task, terminal command 'gulp'.
+gulp.task('default', ['browserSync'], function () { // first task, local server connect & local browser sync.
+    gulp.watch(['base/*', 'tags/*', 'three/*'], ['browserify']); // JS File Browserify.
+    gulp.watch('js/core.js', ['jsmin']); // watching change's JS flie, File Compression.
+    gulp.watch('sass/default.scss', ['sass']); // watching sass file save's auto compile.
+    gulp.watch('css/default.css', ['autoprefixer']); // watching change's CSS flie. add vendor prefix automatically.
+    gulp.watch('css/default.css', ['cssmin']); // watching change's CSS flie, File Compression.
+    gulp.watch(noCompressionImagesFold, ['compressionImages']); // watching noCompressionImages fold changed images, compression images.
+    //gulp.watch('**/*', ['rename']); // watching change's HTML flie. Rename PHP file.
+    //gulp.watch('**/*', ['delete']); // watching rename PHP file. delet HTML file.
+    gulp.watch(upLoadFile, ['ftpUpLoad']); // watching file save's auto ftp upload.
+    gulp.watch(upLoadFile, ['localBrowserReload']); // watching file save's local browser reload.
 });
