d002f71658887acb2f89d345a75091ce5500338e
kojiyamauchi <all.the.small.things@me.com>
2017-04-30 22:10:10 +0900

    Stop Require dat.GUI.

--- three/sample4.js
+++ three/sample4.js
@@ -3,7 +3,7 @@ var THREE = require('three'),
     // Require stats.js.
     Stats = require('stats.js'),
     // Require dat.GUI.
-    datGUI = require('dat.gui'),
+    //datGUI = require('dat.gui'),
     // Require Color JS Library.
     chroma = require('chroma-js');
 
